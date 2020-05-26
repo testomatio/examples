33067504a4ed5ae756cae0a7b8ff8069ed4c1547
Koushik Mohan <koushikmohan1996@gmail.com>
2020-05-27 01:14:12 +0530

    Add testcafe examples (#6)

---
+++ testcafe/tests/client-scripts/inject-a-nodejs-module.js
@@ -0,0 +1,8 @@
+fixture `Fixture`
+    .clientScripts({ module: 'lodash'});
+
+test('Inject a Node.js module into a tested page', async t => {
+    const result = await t.eval(() => _.defaults({ 'a': 1 }, { 'a': 3, 'b': 2 }));
+
+    await t.expect(result).eql({ a: 1, b: 2 });
+});
---
+++ testcafe/tests/page-manipulation/reload-page.js
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe';
+
+fixture `Page manipulation`
+    .page('https://devexpress.github.io/testcafe/example/');
+
+test('Reload the tested page', async t => {
+    await t.typeText('#developer-name', 'Peter Parker');
+
+    await t.eval(() => location.reload(true));
+
+    await t
+        .wait(3000)
+        .expect(Selector('#developer-name').value).eql('');
+});
