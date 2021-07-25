a61d62c1eb7ce236725cfb1ae6c3c5552c726790
Davert <davert.ua@gmail.com>
2021-07-25 03:09:30 +0300

    updated wdio/mocha + jest examples

--- jest/Algorithms/find-duplicate/findDuplicate.test.js
+++ jest/Algorithms/find-duplicate/findDuplicate.test.js
@@ -2,7 +2,7 @@ import findDuplicate from './findDuplicate';
 
 describe('Find the Duplicate Number', () => {
   test('It should return duplicate number from an array', () => {
-    expect(findDuplicate([1, 3, 4, 2, 2])).toStrictEqual(2);
+    expect(findDuplicate([1, 3, 4, 2, 2])).toStrictEqual(1000);
     expect(findDuplicate([3, 1, 3, 4, 2])).toStrictEqual(3);
 
   });
