a04728981400087d69b8999b1bee086bd793daf0
Michael Bodnarchuk <DavertMik@users.noreply.github.com>
2023-02-13 13:19:35 +0200

    Update and rename rpConfig.js to reporter.conf.js

--- mocha-typescript/reporter.conf.js
+++ mocha-typescript/reporter.conf.js
@@ -1,6 +1,6 @@
 module.exports = {
   reporterEnabled: 'spec, @testomatio/reporter/lib/adapter/mocha.js',
   testomatioReporterLibAdapterMochaJsReporterOptions: {
-    apiKey: process.env.TESTOMAT_API_KEY
+    apiKey: process.env.TESTOMATIO
   }
 }
