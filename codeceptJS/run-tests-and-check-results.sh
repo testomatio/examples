8b699b0f32d84108349fa43bf6530840adcd73a5
Vitaliy Mykhailiuk AQA <mihaylukvv@gmail.com>
2023-07-20 15:29:39 +0300

    [EXAMPLE-REPORTING] codeceptJS: number of failed tests=12

--- codeceptJS/run-tests-and-check-results.sh
+++ codeceptJS/run-tests-and-check-results.sh
@@ -3,8 +3,8 @@
 npx codeceptjs run > test-results.txt
 fail_tests=$(grep -oE '\b[0-9]+\s+failed\b' test-results.txt | grep -oE '[0-9]+')
 echo "Number of failed tests: $fail_tests"
-if [ "$fail_tests" -gt 9 ]; then
-  echo "More than 9 failed tests. Exiting with error."
+if [ "$fail_tests" -gt 12 ]; then
+  echo "More than 12 failed tests. Exiting with error."
   exit 1
 else
   exit 0
