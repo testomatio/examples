9b2db9becdb56f2bc36343e0c4eff5c353e64be7
Vitaliy Mykhailiuk AQA <mihaylukvv@gmail.com>
2023-08-21 11:39:49 +0300

    [EXAMPLES] remmove WDIO & protrator examples(old)

--- webdriverio-mocha/test/pages/components/home_modal.component.js
+++
@@ -1,8 +0,0 @@
-export default class ModalComponent {
-  constructor(element) {
-    this.element = element;
-  }
-
-  get title() {return this.element.$('h3').getText();}
-  get content() {return this.element.$('p').getText();}
-}
