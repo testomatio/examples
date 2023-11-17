a983d2bfb49d953834fbb1d992f2b1690013ff02
Vitaliy Mykhailiuk AQA <mihaylukvv@gmail.com>
2023-11-17 10:51:14 +0200

    [EXAMPLE CI] Playwright CI: updated number of the failed tests

--- playwright/run-tests-and-check-results.sh
+++ playwright/run-tests-and-check-results.sh
@@ -3,8 +3,8 @@
 npx playwright test > test-playwright-results.txt
 fail_tests=$(grep -oE '\b[0-9]+\s+failed\b' test-playwright-results.txt | grep -oE '[0-9]+')
 echo "Number of failed tests: $fail_tests"
-if [ "$fail_tests" -gt 2 ]; then
-  echo "More than 2 failed tests. Exiting with error."
+if [ "$fail_tests" -gt 7 ]; then
+  echo "More than 7 failed tests. Exiting with error."
   exit 1
 else
   exit 0
