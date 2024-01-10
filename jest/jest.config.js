25eaaca6ce207bdc436d8f9a0a7d19ce79b6802d
Oleksandr Pelykh <olexandr13@gmail.com>
2024-01-10 21:14:46 +0200

    upgrade jest version

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   testEnvironment: "node",
   reporters: ['default', ['@testomatio/reporter/lib/adapter/jest.js', { apiKey: process.env.TESTOMATIO }]],
