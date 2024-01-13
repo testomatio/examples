1124143a34e6c9049f826d94a8c1cfd3f8a45f39
Oleksandr Pelykh <olexandr13@gmail.com>
2024-01-13 12:14:14 +0200

    remove cypress plugin usage duplication

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,8 +5,9 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      require('@testomatio/reporter/lib/adapter/cypress-plugin')(on, config);
-      return require('./cypress/plugins/index.js')(on, config)
+      // require('@testomatio/reporter/lib/adapter/cypress-plugin')(on, config);
+      // return require('./cypress/plugins/index.js')(on, config)
+      return require('@testomatio/reporter/lib/adapter/cypress-plugin')(on, config)
     },
   },
 })
