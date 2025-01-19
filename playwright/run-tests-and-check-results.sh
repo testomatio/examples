a8fd1dc75f22a3ffe9ee5b1cd5ace193617fcd50
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2025-01-19 15:58:28 +0200

    test

--- playwright/run-tests-and-check-results.sh
+++ playwright/run-tests-and-check-results.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npx playwright test > test-playwright-results.txt
+npx TESTOMATIO=tstmt_qmj-XjR8NsE8LGdcn_7PSONmm3zj6swJ1g1698341001 TESTOMATIO_URL=https://beta.testomat.io playwright test > test-playwright-results.txt
 fail_tests=$(grep -oE '\b[0-9]+\s+failed\b' test-playwright-results.txt | grep -oE '[0-9]+')
 echo "Number of failed tests: $fail_tests"
 if [ "$fail_tests" -gt 7 ]; then
