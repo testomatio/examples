8b3c1a9d4b4270a165c20e5dbef874ef73325e3c
Oleksandr Pelykh <oleksandr.pelykh@gmail.com>
2025-03-23 18:00:35 +0200

    remove test ids

--- nightwatch/nightwatch/unit-filename1.test.ts
+++ nightwatch/nightwatch/unit-filename1.test.ts
@@ -1,19 +1,19 @@
 import { assert } from "nightwatch";
 
 
-it('test inside a file @T5a545278', function () {
+it('test inside a file', function () {
   assert.ok(true);
 });
 
 describe('suite name for positive tests', function () {
   this.tags = ['demo'];
 
-  it('test inside a suite @T85167c7a', function () {
+  it('test inside a suite', function () {
     assert.ok(true);
   });
 
   describe('nested suite name', function () {
-    it('test inside nested suite @T63699dc1', function () {
+    it('test inside nested suite', function () {
       assert.ok(true);
     });
   });
