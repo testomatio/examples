3eac7b099c68d779da4cb5d3b11a883333691ced
Yevhenii Vlasenko <vlasenkoevy@gmail.com>
2025-06-25 16:00:35 +0300

    cucumber demo fixes

--- java-cucumber-maven/src/test/java/TestRunner.java
+++ java-cucumber-maven/src/test/java/TestRunner.java
@@ -5,14 +5,13 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "steps",
+        glue = {"steps"},
         plugin = {
                 "pretty",
                 "json:target/cucumber-reports/cucumber.json",
                 "html:target/cucumber-reports/report",
                 "com.testomatio.reporter.core.framework_integration.CucumberListener"
-        },
-        tags = "not @ignore"
+        }
 )
-public class RunnerTest {
+public class TestRunner {
 }
