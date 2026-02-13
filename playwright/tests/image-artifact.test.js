8fcd21e1345c5d43770310a323bf2d69cb5f118e
YanaBaranova0909 <qa.yana.baranova@gmail.com>
2026-02-13 08:53:18 +0100

    test update

--- playwright/tests/image-artifact.test.js
+++ playwright/tests/image-artifact.test.js
@@ -1,8 +1,10 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import testomat from '@testomatio/reporter';
 
 test.describe('upload artifact 2 @S5034889c', () => {
-  test(`upload image @T47b21888`, async () => {
+  test('upload image @T47b21888', async () => {
     testomat.artifact('artifacts/artifact-test-image.png');
+
+    expect(false).toBe(true);
   });
 });
